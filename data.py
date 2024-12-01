34ac8bc09b742795730ae87645329a6163fc7803
maximiliangoepfert <goepfert_max@web.de>
2024-12-01 16:39:49 +0100

    Added: data analysis Energieverbrauch Bundesland Industrie

--- data.py
+++ data.py
@@ -12,12 +12,9 @@ def plotData(dataset, zeit_spalte, last_spalte):
 
     # Maximale Breite einer Spalte erhöhen
     pd.set_option('display.width', 1000)
-    # Umwandlung der Zeit-Spalte in datetime, zur Sicherheit
-    dataset[zeit_spalte] = pd.to_datetime(dataset[zeit_spalte], dayfirst=True, errors='raise')
-    dataset[last_spalte] = dataset[last_spalte].str.replace('.', '', regex=False)  # Tausendertrennzeichen entfernen
-    dataset[last_spalte] = dataset[last_spalte].str.replace(',', '.',
-                                                      regex=False)  # Dezimal-Komma durch Dezimal-Punkt ersetzen
-    dataset[last_spalte] = pd.to_numeric(dataset[last_spalte])
+
+    # Clean data
+    cleanData(dataset, zeit_spalte, last_spalte)
 
     # Plot erstellen
     plt.figure(figsize=(12, 6))  # Größe des Plots festlegen
@@ -34,6 +31,20 @@ def plotData(dataset, zeit_spalte, last_spalte):
     # Plot anzeigen
     plt.show()
 
+def cleanData(dataset, zeit_spalte, last_spalte):
+    # Umwandlung der Zeit-Spalte in datetime, zur Sicherheit
+    dataset[zeit_spalte] = pd.to_datetime(dataset[zeit_spalte], dayfirst=True, errors='raise')
+    dataset[last_spalte] = dataset[last_spalte].str.replace('.', '', regex=False)  # Tausendertrennzeichen entfernen
+    dataset[last_spalte] = dataset[last_spalte].str.replace(',', '.', regex=False)  # Dezimal-Komma durch Dezimal-Punkt ersetzen
+    dataset[last_spalte] = pd.to_numeric(dataset[last_spalte])
+
+    # Prüfen, ob ungültige Werte (NaN) existieren
+    missing_values1 = dataset[zeit_spalte].isnull().sum()
+    print(f"Fehlende Werte in der Spalte '{zeit_spalte}': {missing_values1}")
+    # Prüfen, ob ungültige Werte (NaN) existieren
+    missing_values2 = dataset[last_spalte].isnull().sum()
+    print(f"Fehlende Werte in der Spalte '{last_spalte}': {missing_values2}")
+
 
 if __name__ == "__main__":
     dataPath = "Realisierter_Stromverbrauch_201701010000_202301010000_Tag.csv"
@@ -42,46 +53,15 @@ if __name__ == "__main__":
     last_spalte = "Gesamt (Netzlast) [MWh] Berechnete Auflösungen"
     plotData(data,zeit_spalte, last_spalte)
 
-"""
-target_column = 'Gesamt (Netzlast) [MWh] Originalauflösungen'
-# Prüfen, ob ungültige Werte (NaN) existieren
-missing_values = data[target_column].isnull().sum()
-print(f"Fehlende Werte in der Spalte '{target_column}': {missing_values}")
+    varianz = data[last_spalte].var()
+    print(f"Die Varianz der Netzlast beträgt: {varianz}")
 
-# Ungültige Werte entfernen (optional, wenn sie vorhanden sind)
-data = data.dropna(subset=[target_column])
-# Sicherstellen, dass alle Strings bereinigt werden
-data[target_column] = data[target_column].str.replace('.', '', regex=False)  # Tausendertrennzeichen entfernen
-data[target_column] = data[target_column].str.replace(',', '.', regex=False)  # Dezimal-Komma durch Dezimal-Punkt ersetzen
+    std = data[last_spalte].std()
+    print(f"Die Standartabweichung der Netzlast beträgt: {std}")
+
+    # Größten Wert in der Zielspalte anzeigen
+    max_value = data[last_spalte].max()
+    print(f"Der größte Wert in der Spalte {last_spalte} ist: {max_value}")
 
 
-#print(data.head())
-#print(data.columns)
-data[target_column] = pd.to_numeric(data[target_column])
-
-
-varianz = data[target_column].var()
-print(f"Die Varianz der Netzlast beträgt: {varianz}")
-
-std = data[target_column].std()
-print(f"Die Standartabweichung der Netzlast beträgt: {std}")
-
-# Größten Wert in der Zielspalte anzeigen
-max_value = data[target_column].max()
-
-print(f"Der größte Wert in der Spalte {target_column} ist: {max_value}")
-
-# Sicherstellen, dass die Zeitspalte im Datumsformat ist
-#print(data.dtypes)  # Zeigt den Datentyp der Spalten
-#print(data.head())  # Zeigt die ersten Zeilen der DataFrame
-print(data[['Datum von', 'Gesamt (Netzlast) [MWh] Originalauflösungen']].isnull().sum())  # Zählt Fehlwerte in relevanten Spalten
-
-
-#print(data.head())
-#print(data.columns)
-
-#sampled_data = data[::100]
-#filtered_data = data[(data['Datum von'] >= '2019-01-01 00:00') & (data['Datum von'] <= '2019-01-31 00:00')]
-
-"""
-# TO DO Varianz, Standartabweichung, Variationskoeffizent Zusammenhänge: Kovarianz/Korrelation
+# TO DO Variationskoeffizent Zusammenhänge: Kovarianz/Korrelation, Korrelationskoeffizient
