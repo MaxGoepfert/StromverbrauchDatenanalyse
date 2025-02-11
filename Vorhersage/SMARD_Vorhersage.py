854d6edba53260871cc45832cdb115f7951c11b9
Maxlcoding <goepfert_max@web.de>
2025-02-11 15:58:07 +0100

    Updates SMARD_Vorhersage.py

--- Vorhersage/SMARD_Vorhersage.py
+++ Vorhersage/SMARD_Vorhersage.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
-from Vorhersage.config import DE_STROM_DATA_PATH, DE_STROM_PROG_DATA_PATH
+import config
 from matplotlib import pyplot as plt
 
 ZEIT_SPALTE = "Datum von"
@@ -13,7 +13,11 @@ def cleanData(data):
     # Umwandlung Integer
     dataset[VERBRAUCH_SPALTE] = dataset[VERBRAUCH_SPALTE].str.replace('.', '', regex=False)  # Tausendertrennzeichen entfernen
     dataset[VERBRAUCH_SPALTE] = dataset[VERBRAUCH_SPALTE].str.replace(',', '.', regex=False)  # Dezimal-Komma durch Dezimal-Punkt ersetzen
+    dataset[VERBRAUCH_SPALTE] = dataset[VERBRAUCH_SPALTE].replace("-", np.nan) # Es gibt einen Null Wert in Regelzone TransNetBW/50Hertz
     dataset[VERBRAUCH_SPALTE] = pd.to_numeric(dataset[VERBRAUCH_SPALTE])
+    dataset[VERBRAUCH_SPALTE] = dataset[VERBRAUCH_SPALTE].ffill()
+
+    #dataset = dataset.apply(lambda col: col.fillna(col.mean()), axis=0) # NaN Wert(e) mit Mittelwert auffüllen
 
     # Prüfen, ob ungültige Werte (NaN) existieren
     missing_values1 = dataset[ZEIT_SPALTE].isnull().sum()
@@ -28,9 +32,22 @@ def cleanData(data):
 
 
 if __name__ == "__main__":
+    zone = input("Regelzone oder DE? [de / bw / hertz]\n")
+    dataReal = []
+    dataProg = []
+    if zone == "de":
+        dataReal = pd.read_csv(config.DE_STROM_DATA_PATH, delimiter=';')
+        dataProg = pd.read_csv(config.DE_STROM_PROG_DATA_PATH, delimiter=';')
 
-    dataReal = pd.read_csv(DE_STROM_DATA_PATH, delimiter=';')
-    dataProg = pd.read_csv(DE_STROM_PROG_DATA_PATH, delimiter=';')
+    elif zone == "bw":
+        dataReal = pd.read_csv(config.BW_STROM_DATA_PATH, delimiter=';')
+        dataProg = pd.read_csv(config.BW_STROM_PROG_DATA_PATH, delimiter=';')
+    elif zone == "hertz":
+        dataReal = pd.read_csv(config.HERTZ_STROM_DATA_PATH, delimiter=';')
+        dataProg = pd.read_csv(config.HERTZ_STROM_PROG_DATA_PATH, delimiter=';')
+    else:
+        dataReal = pd.read_csv(config.DE_STROM_DATA_PATH, delimiter=';')
+        dataProg = pd.read_csv(config.DE_STROM_PROG_DATA_PATH, delimiter=';')
 
     ### Clean data
     datasetReal = cleanData(dataReal)
@@ -59,10 +76,10 @@ if __name__ == "__main__":
     plt.plot(datasetProg.index, y_prog, label='SMARD Vorhersagen', color='red', linestyle='--')
 
     # Achsen und Titel
-    plt.ylim(0.5e6, 2e6)
+
     plt.xlabel('Datum')
     plt.ylabel('Stromverbrauch (in Mio MWh)')
-    plt.title('Tatsächliche Werte vs. SMARD Vorhersagen für 2023')
+    plt.title(f'Tatsächliche Werte vs. SMARD Vorhersagen für 2023 (zone: {zone})')
 
     # anzeigen
     plt.legend()
--- Vorhersage/config.py
+++ Vorhersage/config.py
@@ -1,20 +1,21 @@
 import os
 
-# Beispiel für die Konfiguration des Dateipfads
+# Pfad zum Verzeichnis des Projekts
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# Eventuelle Lösung für Dateipfad der config.py: BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 dataPath = "Realisierter_Stromverbrauch_2017_2024_Tag_de.csv"
 dataPath2 = "Realisierter_Stromverbrauch_2017_2024_Tag_50Hertz.csv"
 dataPath3 = "Realisierter_Stromverbrauch_2017_2024_Tag_BW.csv"
 dataPath5 = "Prognostizierter_Stromverbrauch_2023_2024_Tag_de.csv"
+dataPath6 = "Prognostizierter_Stromverbrauch_2023_2024_Tag_TransNetBW.csv"
+dataPath7 = "Prognostizierter_Stromverbrauch_2023_2024_Tag_50Hertz.csv"
 
 WEATHER_DATA_PATH = os.path.join(BASE_PATH, 'Vorhersage', 'Wetterdaten')
 DE_STROM_DATA_PATH = os.path.join(BASE_PATH, 'Vorhersage', 'data', dataPath)
 DE_STROM_PROG_DATA_PATH = os.path.join(BASE_PATH, 'Vorhersage', 'data', dataPath5)
 HERTZ_STROM_DATA_PATH = os.path.join(BASE_PATH, 'Vorhersage', 'data', dataPath2)
 BW_STROM_DATA_PATH = os.path.join(BASE_PATH, 'Vorhersage', 'data', dataPath3)
-
-
+BW_STROM_PROG_DATA_PATH = os.path.join(BASE_PATH, 'Vorhersage', 'data', dataPath6)
+HERTZ_STROM_PROG_DATA_PATH = os.path.join(BASE_PATH, 'Vorhersage', 'data', dataPath7)
 
 
